0291337345cf87e38a0ff2bb19486711ff0797e8
lokendra <lokendrachaulagain803@gmail.com>
2022-03-30 17:04:20 +0545

    featured

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,15 +1,34 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
-import React from 'react'
+import React, { useRef } from 'react'
 import ListItem from '../listItem/ListItem'
 import "./list.scss"
 
 function List() {
+
+    //react hooks
+    const listRef= useRef()
+
+
+    // function for onClick event
+    const handleClick = (direction) => {
+
+        let distance = listRef.current.getBoundingClientReact().x - 50
+        if (direction === 'left') {
+            listRef.current.style.transform = `translateX( ${230 + distance}px)`
+
+        }
+        console.log(distance)
+
+    }
+
+
     return (
         <div className='list'>
             <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
-                <ArrowBackIosOutlined className='sliderArrow left' />
-                <div className="container">
+                <ArrowBackIosOutlined className='sliderArrow left' onClick={() => handleClick("left")} />
+                {/* onClick ma yo function call gerne */}
+                <div className="container" ref={listRef} >
                     <ListItem />
                     <ListItem />
                     <ListItem />
@@ -29,7 +48,8 @@ function List() {
                     <ListItem />
                     <ListItem />
                 </div>
-                <ArrowForwardIosOutlined className='sliderArrow right' />
+                <ArrowForwardIosOutlined className='sliderArrow right' onClick={() => handleClick("right")} />
+                {/* onClick ma yo function call gerne */}
             </div>
         </div>
     )
