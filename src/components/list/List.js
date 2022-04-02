fd001aa86f0f2c835b2dac4f6cb5631790e6709f
lokendra <lokendrachaulagain803@gmail.com>
2022-04-02 08:10:09 +0545

    scrolling netflix style

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,58 +1,139 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
-import React, { useRef } from 'react'
-import ListItem from '../listItem/ListItem'
-import "./list.scss"
+import { useRef, useState } from "react";
+import ListItem from "../listItem/ListItem";
+import "./list.scss";
 
-function List() {
+export default function List() {
+    const [isMoved, setIsMoved] = useState(false);
+    const [slideNumber, setSlideNumber] = useState(0);
 
-    //react hooks
-    const listRef= useRef()
+    const listRef = useRef();
 
-
-    // function for onClick event
     const handleClick = (direction) => {
-
-        let distance = listRef.current.getBoundingClientReact().x - 50
-        if (direction === 'left') {
-            listRef.current.style.transform = `translateX( ${230 + distance}px)`
-
+        setIsMoved(true);
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+        if (direction === "left" && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1);
+            listRef.current.style.transform = `translateX(${230 + distance}px)`;
         }
-        console.log(distance)
-
-    }
-
-
+        if (direction === "right" && slideNumber < 5) {
+            setSlideNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+        }
+    };
     return (
-        <div className='list'>
+        <div className="list">
             <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
-                <ArrowBackIosOutlined className='sliderArrow left' onClick={() => handleClick("left")} />
-                {/* onClick ma yo function call gerne */}
-                <div className="container" ref={listRef} >
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
+                <ArrowBackIosOutlined
+                    className="sliderArrow left"
+                    onClick={() => handleClick("left")}
+                    style={{ display: !isMoved && "none" }}
+                />
+                <div className="container" ref={listRef}>
+                    <ListItem index={0} />
+                    <ListItem index={1} />
+                    <ListItem index={2} />
+                    <ListItem index={3} />
+                    <ListItem index={4} />
+                    <ListItem index={5} />
+                    <ListItem index={6} />
+                    <ListItem index={7} />
+                    <ListItem index={8} />
+                    <ListItem index={9} />
                 </div>
-                <ArrowForwardIosOutlined className='sliderArrow right' onClick={() => handleClick("right")} />
-                {/* onClick ma yo function call gerne */}
+                <ArrowForwardIosOutlined
+                    className="sliderArrow right"
+                    onClick={() => handleClick("right")}
+                />
             </div>
         </div>
-    )
+    );
 }
 
-export default List
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MY CODE ------------------------------------------------
+
+
+// import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
+// import React, { useRef } from 'react'
+// import ListItem from '../listItem/ListItem'
+// import "./list.scss"
+
+
+// function List() {
+
+//     const listRef = useRef()
+
+
+//     // function for onClick event
+//     const handleClick = (direction) => {
+
+//         let distance = listRef.current.getBoundingClientReact() //to find distance from left
+
+//         if (direction === 'left') {
+//             listRef.current.style.transform = `translateX(230px)`
+//             // ( ${230 + distance}px)`
+
+//         }
+//         console.log(distance)
+//     }
+
+//     //----------------------------------------
+
+//     return (
+//         <div className='list'>
+//             <span className="listTitle">Continue to watch</span>
+//             <div className="wrapper">
+//                 <ArrowBackIosOutlined className='sliderArrow left' onClick={() => handleClick("left")} />
+//                 {/* onClick ma yo function call gerne */}
+//                 <div className="container" ref={listRef} >
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                     <ListItem />
+//                 </div>
+//                 <ArrowForwardIosOutlined className='sliderArrow right' onClick={() => handleClick("right")} />
+//                 {/* onClick ma yo function call gerne */}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default List
--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -1,11 +1,48 @@
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from '@mui/icons-material'
 import React from 'react'
 import "./listItem.scss"
+import { useState } from 'react'
 
-function ListItem() {
+function ListItem({index}) {
+    //
+    const [isHovered, setIsHovered] = useState(false)
+
+
     return (
-        <div className='listItem'>
-            ListItem
+
+        <div className='listItem' 
+    style={{left:isHovered && index *225 -50} }
+        onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <img src="https://images6.alphacoders.com/114/thumbbig-1141749.webp" alt="" />
+
+
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrow />
+                    <Add />
+                    <ThumbUpAltOutlined />
+                    <ThumbDownAltOutlined />
+                </div>
+
+                <div className="itemInfoTop">
+                    <span>1 hour 45 mins</span>
+                    <span className='ageLimit'>+18</span>
+                    <span>2002</span>
+
+                </div>
+
+                <div className="description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptates asperiores placeat totam nulla. Eaque.
+                </div>
+
+                <div className="genere">Action</div>
+
+            </div>
         </div>
+
+
+
+
     )
 }
 
