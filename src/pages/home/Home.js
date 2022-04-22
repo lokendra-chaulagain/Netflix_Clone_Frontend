483ee672632352b35f8bdb66d480bcce4ba10e01
lokendra <lokendrachaulagain803@gmail.com>
2022-04-22 20:27:57 +0545

    routing

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,8 +3,29 @@ import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 const Home = ({ type }) => {
+
+  const [lists, setLists] = useState([])
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(`lists${type && "?type=" + type}`)
+
+      } catch (error) {
+        console.log(error);
+      }
+
+
+
+    }
+
+  })
+
+
   return (
     <div className="home">
       <Navbar />
