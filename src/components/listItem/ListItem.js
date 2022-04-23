1ed6545d865dc2536f9fc614cbbcf0f249782660
lokendra <lokendrachaulagain803@gmail.com>
2022-04-23 11:45:52 +0545

    movies fetch from api

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -2,15 +2,14 @@ import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from '@mui/i
 import React from 'react'
 import "./listItem.scss"
 import { useState, useEffect } from 'react'
-import axios from 'axios';
+import axios from 'axios'
 
 function ListItem({ index, item }) {
-    //hover effect
-    const [isHovered, setIsHovered] = useState(false);
-    
+    //Hover effect
+    const [isHovered, setIsHovered] = useState(false)
 
 
-    //
+    //For getting any random lists with query
     const [movie, setMovie] = useState({})
     useEffect(() => {
         const getMovie = async () => {
@@ -20,9 +19,9 @@ function ListItem({ index, item }) {
                         token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjFmOTMxM2RmMGYzZDYzNTg3NDA2MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MDY3MTAwMCwiZXhwIjoxNjUxNTM1MDAwfQ.HOeFPw__H4xc80CJOX2bUcLuzex9W-tD1-ZiqZA5By8 "
                     }
                 })
-                // console.log(res.data);
+                // console.log(res);
                 setMovie(res.data)
-                
+
             } catch (error) {
                 console.log(error)
             }
@@ -31,42 +30,135 @@ function ListItem({ index, item }) {
     }, [item])//whenever we change the item fire this useEffect
 
 
-
     return (
         <div className='listItem'
             style={{ left: isHovered && index * 225 - 50 }}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}>
-
+            onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             <img src={movie.img} alt="" />
 
 
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrow className='icon' />
+                    <Add className='icon' />
+                    <ThumbUpAltOutlined className='icon' />
+                    <ThumbDownAltOutlined className='icon' />
+                </div>
 
-            {isHovered && (
-                <>
-                    <video src={movie.trailer} autoPlay={true} loop />
-                    <div className="itemInfo">
-                        <div className="icons">
-                            <PlayArrow className='icon' />
-                            <Add className='icon' />
-                            <ThumbUpAltOutlined className='icon' />
-                            <ThumbDownAltOutlined className='icon' />
-                        </div>
+                <div className="itemInfoTop">
+                    <span>{movie.duration || '2 hour 35 min'}</span>
+                    <span className='ageLimit'>+{movie.ageLimit}</span>
+                    <span>{movie.year}</span>
+                </div>
 
-                        <div className="itemInfoTop">
-                            <span>{movie.duration}</span>
-                            <span className='ageLimit'>+{movie.ageLimit} </span>
-                            <span>{movie.year} </span>
-                        </div>
+                <div className="description">{movie.desc} </div>
+                <div className="genere">{movie.genere || 'genera:Action'}</div>
 
-                        <div className="description">{movie.desc}</div>
-
-                        <div className="genere">{movie.genere} </div>
-                    </div>
-                </>
-            )}
+            </div>
         </div>
     )
 }
 
 export default ListItem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from '@mui/icons-material'
+// import React from 'react'
+// import "./listItem.scss"
+// import { useState } from 'react'
+
+
+// function ListItem({  index }) {
+//     //hover effect
+//     const [isHovered, setIsHovered] = useState(false);
+
+
+
+//     //
+//     // const [movie, setMovie] = useState({})
+//     // useEffect(() => {
+//     //     const getMovie = async () => {
+//     //         try {
+//     //             const res = await axios.get("movies/find/" + item, {
+//     //                 headers: {
+//     //                     token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjFmOTMxM2RmMGYzZDYzNTg3NDA2MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MDY3MTAwMCwiZXhwIjoxNjUxNTM1MDAwfQ.HOeFPw__H4xc80CJOX2bUcLuzex9W-tD1-ZiqZA5By8 "
+//     //                 }
+//     //             })
+//     //             // console.log(res.data);
+//     //             setMovie(res.data)
+
+//     //         } catch (error) {
+//     //             console.log(error)
+//     //         }
+//     //     }
+//     //     getMovie()
+//     // }, [item])//whenever we change the item fire this useEffect
+
+
+
+//     return (
+//         <div className='listItem'
+//             style={{ left: isHovered && index * 225 - 50 }}
+//             onMouseEnter={() => setIsHovered(true)}
+//             onMouseLeave={() => setIsHovered(false)}>
+
+//             <img src="" alt="" />
+
+
+
+//             {isHovered && (
+//                 <>
+//                     <video src="" autoPlay={true} loop />
+//                     <div className="itemInfo">
+//                         <div className="icons">
+//                             <PlayArrow className='icon' />
+//                             <Add className='icon' />
+//                             <ThumbUpAltOutlined className='icon' />
+//                             <ThumbDownAltOutlined className='icon' />
+//                         </div>
+
+//                         <div className="itemInfoTop">
+//                             <span>12 hour</span>
+//                             <span className='ageLimit'>+16 </span>
+//                             <span> </span>
+//                         </div>
+
+//                         <div className="description">desc</div>
+
+//                         <div className="genere">genere </div>
+//                     </div>
+//                 </>
+//             )}
+//         </div>
+//     )
+// }
+
+// export default ListItem
